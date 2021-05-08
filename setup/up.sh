bf8ff080996f17dffb1a24d49692d07d52cfdf7a
abin <1506376703@qq.com>
2021-05-08 21:40:30 +0800

    update scripts

--- setup/up.sh
+++ setup/up.sh
@@ -7,7 +7,7 @@ user=bin_tang
 machines=(apt050.apt.emulab.net	apt007.apt.emulab.net)
 
 for machine in ${machines[*]};do
-    echo "copy to ${machine}..."
+    echo "copy ${project} to ${machine}..."
     ssh ${user}@${machine} "pidof activeswap-daemon | xargs kill -s 9"
     ssh ${user}@${machine} "pidof infiniswap-daemon | xargs kill -s 9"
     scp -r ../../${project} ${user}@${machine}:~/ >/dev/null 2>&1
