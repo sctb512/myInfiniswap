ca33ccb886574ce2870ff59740074cf908c8a0ee
abin <1506376703@qq.com>
2021-05-10 17:17:25 +0800

    update files

--- exp/dataframe_docker.sh
+++ exp/dataframe_docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+user=bin_tang
+server=128.110.96.50
+server_ib=192.168.0.112
+client_ib=192.168.0.111
+
 output_dir="./result_dataframe"
 if [ ! -d ${output_dir} ]; then
     mkdir -p ${output_dir}
@@ -9,10 +14,33 @@ total_mem=41943040
 docker_name=is_workloads
 echo "total_mem: ${total_mem}"
 
-for local in 100 75 50;do
-    local_mem=`expr ${total_mem} \* 100 / ${local}`
+# ssh ${user}@${server} "pidof infiniswap-daemon | xargs kill -s 9"
+# ssh ${user}@${server} "cd ~/myInfiniswap/setup && ./daemon.sh ${server_ib} 9400" &
+
+# echo "sleep 80s..."
+# sleep 80
+
+for local in 100 75 50 25;do
+    local_mem=`expr ${total_mem} \* ${local} / 100`
     df_num=`expr ${local_mem} / 220851`
     echo "local_mem: ${local_mem}, df_num: ${df_num}"
+
+    file_name="total_mem${total_mem}_local_mem${local_mem}_local${local}.txt"
+
+    if [ -f ${output_dir}/${file} ];then
+        continue
+    fi
+
+    ssh ${user}@${server} "pidof infiniswap-daemon | xargs kill -s 9"
+    ssh ${user}@${server} "cd ~/myInfiniswap/setup && ./daemon.sh ${server_ib} 9400" &
+
+    echo "sleep 80s..."
+    sleep 80
+
+    cd ../setup
+    ./bd.sh ${client_ib}
+    cd ../exp
+    dmesg | tail
 
     sudo docker stop $(sudo docker ps -a -q)
     sudo docker rm $(sudo docker ps -a -q)
@@ -23,7 +51,11 @@ for local in 100 75 50;do
     echo "install env in docker..."
     sudo docker exec -it ${docker_name} /bin/bash -c "apt-get update && apt-get install python3 -y && python3 -V && apt-get install python3-pip -y && pip3 install pandas" > /dev/null 2>&1
     sudo docker cp dataframe.py ${docker_name}:/root
-    sudo docker exec -it ${docker_name} /bin/bash -c "cd /root && mkdir ${output_dir} && ls && (time python3 dataframe.py ${df_num}) 2> ${output_dir}/total_mem${total_mem}_local_mem${local_mem}_local${local}.txt"
+    sudo docker exec -it ${docker_name} /bin/bash -c "cd /root && mkdir ${output_dir} && ls && (time python3 dataframe.py ${df_num}) 2> ${output_dir}/${file_name}"
 
-    sudo docker cp ${docker_name}:/root/${output_dir}/ ${output_dir}/
+    sudo docker cp ${docker_name}:/root/${output_dir}/ .
+
+    cd ../setup
+    ./old.sh
+    cd ../exp
 done
--- exp/docker_env.sh
+++ exp/docker_env.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+# sudo apt-get remove docker docker-engine docker-ce docker.io
+# sudo apt-get update
+# sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# sudo apt-get update
+# sudo apt-get install -y docker-ce
+
 sudo apt-get install -y docker.io
 
 sudo docker search ubuntu
 sudo docker pull ubuntu
+
+wget https://get.docker.com/builds/Linux/x86_64/docker-1.9.1
+sudo chmod +x docker-1.9.1
+sudo mv docker-1.9.1 /usr/bin/docker
+sudo service docker restart
