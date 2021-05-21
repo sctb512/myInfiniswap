de57b42538bb25a0376ec2333cc5fb81f8195747
abin <1506376703@qq.com>
2021-05-21 17:45:40 +0800

    update files

--- exp/cpu_rate_docker.sh
+++ exp/cpu_rate_docker.sh
@@ -2,11 +2,11 @@
 
 file="$1_cpu_rate.csv"
 
-echo "num,cpu_rate,mm_rate" > file
+echo "num,cpu_rate,mm_rate" > ${file}
 
 i=0
 while [ True ];do
-    echo "${i},`sudo docker stats --no-stream is_workloads |awk 'NR==2 {printf("%s,%s\n",$3,$7)}'`" >> file
+    echo "${i},`sudo docker stats --no-stream is_workloads |awk 'NR==2 {printf("%s,%s\n",$2,$8)}'`" >> ${file}
     sleep 1
     i=`expr ${i} + 1`
 done
