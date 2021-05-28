b9c1cbd098da946ab35233a7cd1c65d4549ef949
abin <1506376703@qq.com>
2021-05-28 22:41:21 +0800

    update files

--- setup/bd.sh
+++ setup/bd.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+client=111
+
+if [ $1 == "1" ];then
+    echo "server num: 1"
+    client=111
+    echo -e "1\n192.168.0.119" | sudo tee portal.list
+elif [ $1 == "2" ];then
+    echo "server num: 2"
+    client=116
+    echo -e "2\n192.168.0.117\n192.168.0.118" | sudo tee portal.list
+elif [ $1 == "4" ];then
+    echo "server num: 4"
+    client=111
+    echo -e "4\n192.168.0.112\n192.168.0.113\n192.168.0.114\n192.168.0.115" | sudo tee portal.list
+elif [ $1 == "8" ];then
+    echo "server num: 8"
+    client=111
+    echo -e "8\n192.168.0.112\n192.168.0.113\n192.168.0.114\n192.168.0.115\n192.168.0.116\n192.168.0.117\n192.168.0.118\n192.168.0.119" | sudo tee portal.list
+fi
+
+echo "client ip: 192.168.0.${client}"
+
 sudo dmesg -C
 
 cd ../infiniswap_bd
@@ -20,7 +42,7 @@ sudo ./install.sh bd
 echo "confihure ib0 ..."
 sudo /etc/init.d/openibd restart
 
-sudo ./ib_setup.sh $1
+sudo ./ib_setup.sh 192.168.0.${client}
 echo "sleep 10s..."
 sleep 10
 ifconfig ib0 | grep "inet addr"
--- setup/run_bd.sh
+++ setup/run_bd.sh
@@ -9,29 +9,7 @@ cd ~/myInfiniswap/setup
 
 ./pull.sh
 
-client=111
-
-if [ $1 == "1" ];then
-    echo "server num: 1"
-    client=111
-    echo -e "1\n192.168.0.119" | sudo tee portal.list
-elif [ $1 == "2" ];then
-    echo "server num: 2"
-    client=116
-    echo -e "2\n192.168.0.117\n192.168.0.118" | sudo tee portal.list
-elif [ $1 == "4" ];then
-    echo "server num: 4"
-    client=111
-    echo -e "4\n192.168.0.112\n192.168.0.113\n192.168.0.114\n192.168.0.115" | sudo tee portal.list
-elif [ $1 == "8" ];then
-    echo "server num: 8"
-    client=111
-    echo -e "8\n192.168.0.112\n192.168.0.113\n192.168.0.114\n192.168.0.115\n192.168.0.116\n192.168.0.117\n192.168.0.118\n192.168.0.119" | sudo tee portal.list
-fi
-
-# ./bd.sh 192.168.0.111
-echo "client ip: 192.168.0.${client}"
-./bd.sh 192.168.0.${client}
+./bd.sh $1
 
 # cd ../exp
 # ./dataframe_docker.sh
