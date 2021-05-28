4df0078f1f990a6bf49ac1575d1b029d20446b1e
abin <1506376703@qq.com>
2021-05-28 22:06:31 +0800

    update files

--- setup/run_bd.sh
+++ setup/run_bd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# != 2 ];then
-    echo "useage: $0 ib_addr server_num"
+if [ $# != 1 ];then
+    echo "useage: $0 server_num"
     exit
 fi
 
@@ -9,22 +9,29 @@ cd ~/myInfiniswap/setup
 
 ./pull.sh
 
+client=111
+
 if [ $2 == "1" ];then
     echo "server num: 1"
-    echo -e "1\n192.168.0.12" | sudo tee portal.list
+    client=118
+    echo -e "1\n192.168.0.119" | sudo tee portal.list
 elif [ $2 == "2" ];then
     echo "server num: 2"
-    echo -e "2\n192.168.0.142\n192.168.0.143" | sudo tee portal.list
+    client=116
+    echo -e "2\n192.168.0.117\n192.168.0.118" | sudo tee portal.list
 elif [ $2 == "4" ];then
     echo "server num: 4"
+    client=111
     echo -e "4\n192.168.0.112\n192.168.0.113\n192.168.0.114\n192.168.0.115" | sudo tee portal.list
 elif [ $2 == "8" ];then
     echo "server num: 8"
+    client=111
     echo -e "8\n192.168.0.112\n192.168.0.113\n192.168.0.114\n192.168.0.115\n192.168.0.116\n192.168.0.117\n192.168.0.118\n192.168.0.119" | sudo tee portal.list
 fi
 
 # ./bd.sh 192.168.0.111
-./bd.sh 192.168.0.$1
+echo "client ip: 192.168.0.${client}"
+./bd.sh 192.168.0.${client}
 
 # cd ../exp
 # ./dataframe_docker.sh
