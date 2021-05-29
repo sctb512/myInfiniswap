d97f19d8e378f9f6d8313202fcc8761cbda665d5
abin <1506376703@qq.com>
2021-05-29 10:24:58 +0800

    update files

--- setup/bd.sh
+++ setup/bd.sh
@@ -4,20 +4,20 @@ client=111
 
 if [ $1 == "1" ];then
     echo "server num: 1"
-    client=118
-    echo -e "1\n192.168.0.119" | sudo tee portal.list
+    client=141
+    echo -e "1\n192.168.0.142" | sudo tee portal.list
 elif [ $1 == "2" ];then
     echo "server num: 2"
-    client=116
-    echo -e "2\n192.168.0.117\n192.168.0.118" | sudo tee portal.list
+    client=151
+    echo -e "2\n192.168.0.152\n192.168.0.153" | sudo tee portal.list
 elif [ $1 == "4" ];then
     echo "server num: 4"
-    client=111
-    echo -e "4\n192.168.0.112\n192.168.0.113\n192.168.0.114\n192.168.0.115" | sudo tee portal.list
+    client=161
+    echo -e "4\n192.168.0.162\n192.168.0.163\n192.168.0.164\n192.168.0.165" | sudo tee portal.list
 elif [ $1 == "8" ];then
     echo "server num: 8"
-    client=111
-    echo -e "8\n192.168.0.112\n192.168.0.113\n192.168.0.114\n192.168.0.115\n192.168.0.116\n192.168.0.117\n192.168.0.118\n192.168.0.119" | sudo tee portal.list
+    client=171
+    echo -e "8\n192.168.0.172\n192.168.0.173\n192.168.0.174\n192.168.0.175\n192.168.0.176\n192.168.0.177\n192.168.0.178\n192.168.0.179" | sudo tee portal.list
 fi
 
 echo "client ip: 192.168.0.${client}"
