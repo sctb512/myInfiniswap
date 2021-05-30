f2df6027a58e1ef2d5a9b435d82665e17949722a
abin <1506376703@qq.com>
2021-05-30 11:49:59 +0800

    update files

--- infiniswap_bd/.infiniswap.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/Infiniswap/infiniswap_bd/infiniswap.o := ld -m elf_x86_64  -z max-page-size=0x200000   -r -o /root/Infiniswap/infiniswap_bd/infiniswap.o /root/Infiniswap/infiniswap_bd/is_configfs.o /root/Infiniswap/infiniswap_bd/is_mq.o /root/Infiniswap/infiniswap_bd/is_main.o 
--- infiniswap_bd/.tmp_versions/infiniswap.mod
+++
@@ -1,2 +0,0 @@
-/root/Infiniswap/infiniswap_bd/infiniswap.ko
-/root/Infiniswap/infiniswap_bd/is_configfs.o /root/Infiniswap/infiniswap_bd/is_mq.o /root/Infiniswap/infiniswap_bd/is_main.o
