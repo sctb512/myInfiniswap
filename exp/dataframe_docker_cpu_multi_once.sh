811727c6e8e9499a55967e6a43ec3edf812b243f
abin <1506376703@qq.com>
2021-06-08 11:03:30 +0800

    update files

--- exp/dataframe_docker_cpu_multi_once.sh
+++ exp/dataframe_docker_cpu_multi_once.sh
@@ -46,7 +46,7 @@ fi
 sudo mkdir -p ${output_dir}/${i}
 for local in 100 75 50 25;do
     local_mem=`expr ${total_mem} \* ${local} / 100`
-    df_num=`expr ${local_mem} / 220851`
+    df_num=`expr ${total_mem} / 220851`
     echo "local_mem: ${local_mem}, df_num: ${df_num}"
 
     file="total_mem${total_mem}_local_mem${local_mem}_local${local}.txt"
@@ -84,7 +84,7 @@ for local in 100 75 50 25;do
     sudo docker exec -it ${docker_name} /bin/bash -c "cd /root && mkdir ${output_dir} && ls && (time python3 dataframe.py ${df_num}) 2> ${output_dir}/${file}"
 
     sudo docker cp ${docker_name}:/root/${output_dir}/ ./${output_dir}/${i}/
-    
+
     # cd ../setup
     # ./old.sh
     # cd ../exp
