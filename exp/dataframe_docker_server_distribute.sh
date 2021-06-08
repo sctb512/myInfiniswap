0fedfa2b7e142abc564ed76ecfd1b8669ae76e5e
abin <1506376703@qq.com>
2021-06-08 21:57:54 +0800

    update files

---
+++ exp/dataframe_docker_server_distribute.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+servers_num=$2
+
+once_file="is_once.txt"
+server_distribute="is_server_distribute.txt"
+
+# sudo rm -rf ${output_dir}/*
+
+
+if [ ! -f ${once_file} ]; then
+    echo 1 > ${once_file}
+fi
+i=`cat ${once_file}`
+
+
+cd ../setup
+./run_infiniswap.sh ${servers_num}
+cd ../exp
+./compile_cpu.sh
+./cpu ${cpu_useage} &
+
+
+if [ ! -f ${server_distribute} ]; then
+    head="turn"
+    for k in `seq ${servers_num}`;do
+        head="${head},${k}"
+    done
+    echo ${head} > ${server_distribute}
+fi
+
+sudo fio -filename=/dev/infiniswap0 -direct=1 -iodepth 64 -thread -rw=32 -ioengine=psync -bs=32k -size=16G -numjobs=32 -runtime=30 -group_reporting -name=is_server_tmp.txt
+
+line="${i}"
+ib=212
+for m in `seq ${servers_num}`;do
+    num=`dmesg | grep "] cb->cb_index: ., ip: 192.168.0." | grep ${ib} | wc -l`
+    line="${line},${num}"
+    ib=`expr ${ib} + 1`
+done
+echo ${line} >> ${server_distribute}
+
+i=`expr ${i} + 1`
+echo ${i} > ${once_file}
