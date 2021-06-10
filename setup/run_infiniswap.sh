5446fc19f1db146c8a98eb601d5025ec184a1b4c
abin <1506376703@qq.com>
2021-06-10 10:32:25 +0800

    update files

--- setup/run_infiniswap.sh
+++ setup/run_infiniswap.sh
@@ -7,8 +7,6 @@ echo "StrictHostKeyChecking no" > ~/.ssh/config
 ib=212
 for server in ${servers[*]};do
     echo "cp-${server}, ib: 192.168.0.${ib}, running..."
-    ssh ${user}@128.110.96.${server} "pidof infiniswap-daemon | xargs kill -s 9"
-    ssh ${user}@128.110.96.${server} "pidof activeswap-daemon | xargs kill -s 9"
     ssh ${user}@128.110.96.${server} "ps -ef | grep run_daemon.sh | grep /bin/bash | awk '{print \$2}' | xargs kill -s 9"
     ssh ${user}@128.110.96.${server} "cd ~/myInfiniswap/setup && ./run_daemon.sh ${ib} > /dev/null 2>&1 &" 
     ib=`expr ${ib} + 1`
