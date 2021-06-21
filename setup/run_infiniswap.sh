9be2fa409a80f7523518965f067cd30d8e829278
abin <1506376703@qq.com>
2021-06-21 17:53:09 +0800

    update files

--- setup/run_infiniswap.sh
+++ setup/run_infiniswap.sh
@@ -6,13 +6,14 @@ source servers.sh ${num}
 
 echo "StrictHostKeyChecking no" > ~/.ssh/config
 
-ib=212
-for server in ${servers[*]};do
+for i in ${!servers[@]};do
+    server=${servers}[${i}]
+    ib=${ibs}[${i}]
+
     echo "cp-${server}, ib: 192.168.0.${ib}, running..."
     ssh ${user}@128.110.96.${server} "ps -ef | grep run_daemon.sh | grep /bin/bash | awk '{print \$2}' | xargs kill -s 9"
     sleep 2
     ssh ${user}@128.110.96.${server} "cd ~/myInfiniswap/setup && ./run_daemon.sh ${ib} > /dev/null 2>&1 &" 
-    ib=`expr ${ib} + 1`
 done
 
 echo "sleep 60s..."
@@ -21,7 +22,7 @@ sleep 60
 # echo 0 | sudo tee /proc/sys/kernel/hung_task_timeout_secs
 
 cd ../setup
-./run_bd.sh ${num}
+./run_bd.sh ${num} ${client}
 cd ../exp
 
 sleep 20
--- setup/servers.sh
+++ setup/servers.sh
@@ -4,10 +4,18 @@ user=bin_tang
 
 if [ $1 == "8" ];then
     servers=(55 62 46 63 39 43 48 61)
+    client=211
+    ibs=(212 213 214 215 216 217 218 219)
 elif [ $1 == "4" ];then
     servers=(55 62 46 63)
+    client=211
+    ibs=(212 213 214 215)
 elif [ $1 == "2" ];then
     servers=(55 62)
+    client=211
+    ibs=(212 213)
 elif [ $1 == "1" ];then
     servers=(55)
+    client=211
+    ibs=(212)
 fi
