3fe56f7db443af2e74ee0f9c8216ce97779ae0d7
abin <1506376703@qq.com>
2021-07-26 22:38:33 +0800

    update files

--- setup/servers.sh
+++ setup/servers.sh
@@ -7,15 +7,15 @@ if [ $1 == "8" ];then
     client=211
     ibs=(212 213 214 215 216 217 218 219)
 elif [ $1 == "4" ];then
-    servers=(55 62 46 63)
+    servers=(01 08 04 50)
     client=211
     ibs=(212 213 214 215)
 elif [ $1 == "2" ];then
-    servers=(55 62)
+    servers=(01 08)
     client=211
     ibs=(212 213)
 elif [ $1 == "1" ];then
-    servers=(55)
+    servers=(01)
     client=211
     ibs=(212)
 fi
