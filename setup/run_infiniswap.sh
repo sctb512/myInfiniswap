02ff5ab870381375fbbca67f3c5822d52f59e70e
abin <1506376703@qq.com>
2021-07-28 22:06:05 +0800

    update files

--- setup/run_infiniswap.sh
+++ setup/run_infiniswap.sh
@@ -8,12 +8,13 @@ echo "StrictHostKeyChecking no" > ~/.ssh/config
 
 for i in ${!servers[@]};do
     server=${servers[${i}]}
+    ip=${ips[${i}]}
     ib=${ibs[${i}]}
 
     echo "cp-${server}, ib: 192.168.0.${ib}, running..."
-    ssh ${user}@128.110.96.${server} "ps -ef | grep run_daemon.sh | grep /bin/bash | awk '{print \$2}' | xargs kill -s 9"
+    ssh ${user}@128.110.96.${ip} "ps -ef | grep run_daemon.sh | grep /bin/bash | awk '{print \$2}' | xargs kill -s 9"
     sleep 2
-    ssh ${user}@128.110.96.${server} "cd ~/myInfiniswap/setup && ./run_daemon.sh ${ib} > /dev/null 2>&1 &" 
+    ssh ${user}@128.110.96.${ip} "cd ~/myInfiniswap/setup && ./run_daemon.sh ${ib} > /dev/null 2>&1 &" 
 done
 
 echo "sleep 60s..."
