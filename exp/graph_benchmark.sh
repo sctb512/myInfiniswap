651c473fb0b54aab690f235b0f7ed54d251e76f6
abin <1506376703@qq.com>
2021-08-01 20:32:03 +0800

    update files

--- exp/graph_benchmark.sh
+++ exp/graph_benchmark.sh
@@ -9,13 +9,12 @@ datas=(amazon.txt enron.txt google.txt pokec.txt)
 
 sudo docker exec -i ${docker_name} /bin/bash -c "cd /root/graph-benchmarks/output/ && rm -rf *"
 
-eval "$(conda shell.bash hook)"
 for code in ${codes[*]};do
     for data in ${datas[*]};do
         cname=`echo ${code} | awk -F. '{print $1}'`
         outfile="output/${pfx}_${code}_${data}"
         # echo output/${pfx}_${cname}_${data}
-        sudo docker exec -i ${docker_name} /bin/bash -c "source /etc/profile && conda activate && cd /root/graph-benchmarks && bash run_profiler.sh code/${code} data/${data} 100 ${outfile}"
+        sudo docker exec -i ${docker_name} /bin/bash -c "eval "$(conda shell.bash hook)" && source /etc/profile && conda activate && cd /root/graph-benchmarks && bash run_profiler.sh code/${code} data/${data} 100 ${outfile}"
     done
 done
 
