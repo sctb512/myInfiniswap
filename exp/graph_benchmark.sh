378ebd8b0ce5ca095b0fd2f914629b34a4c8dfef
abin <1506376703@qq.com>
2021-08-01 20:24:36 +0800

    update files

--- exp/graph_benchmark.sh
+++ exp/graph_benchmark.sh
@@ -7,10 +7,16 @@ pfx=is
 codes=(graphtool_profile.py lightgraphs.jl networkx_profile.py igraph_profile.py networkit_profile.py snap_profile.py)
 datas=(amazon.txt enron.txt google.txt pokec.txt)
 
+sudo docker exec -i ${docker_name} /bin/bash -c "cd /root/graph-benchmarks/output/ && rm -rf *"
+
 for code in ${codes[*]};do
     for data in ${datas[*]};do
         cname=`echo ${code} | awk -F. '{print $1}'`
+        outfile="output/${pfx}_${code}_${data}"
         # echo output/${pfx}_${cname}_${data}
-        sudo docker exec -i ${docker_name} /bin/bash -c "source /etc/profile && conda activate && bash run_profiler.sh code/${code} data/${data} 100 output/${pfx}_${code}_${data}"
+        sudo docker exec -i ${docker_name} /bin/bash -c "source /etc/profile && conda activate && cd /root/graph-benchmarks && bash run_profiler.sh code/${code} data/${data} 100 ${outfile}"
     done
 done
+
+mkdir ${pfx}_graph_benchmark
+sudo docker cp ${docker_name}:/root/graph-benchmarks/output/ ${pfx}_graph_benchmark/
