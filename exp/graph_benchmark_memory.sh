6f05e2217094171b6ac4e67bfa3b2879c839f4ab
abin <1506376703@qq.com>
2021-08-08 10:46:59 +0800

    update files

--- exp/graph_benchmark_memory.sh
+++ exp/graph_benchmark_memory.sh
@@ -2,7 +2,7 @@
 
 headline="graph_benchmark"
 
-headline="type,memory_used(kB)"
+headline="type,repetition,memory_used(kB)"
 
 outfile=${graph_benchmark}_memory.csv
 echo "${headline}" > ${outfile}
@@ -13,30 +13,33 @@ rm -rf test*.csv
 codes=(graphtool_profile.py networkx_profile.py igraph_profile.py networkit_profile.py snap_profile.py)
 # datas=(amazon.txt enron.txt google.txt pokec.txt)
 datas=(amazon.txt google.txt enron.txt)
+repetitions=(20 40 60 80 100)
 
 for code in ${codes[*]};do
     for data in ${datas[*]};do
-    line="${code}_${data}"	
-    for i in `seq 5`;do
-        mem_base=`free | awk '/Mem/ {print $3}'`
-        mem_max=0
-        source /etc/profile && conda activate base && cd ~/graph-benchmarks && bash run_profiler.sh code/${code} data/${data} 100 graph_benchmark_time_tmp.txt &
-        sleep 10
+        for repetition in ${repetitions[*]};do
+        line="${code}_${data},${repetition}"	
+        for i in `seq 5`;do
+            mem_base=`free | awk '/Mem/ {print $3}'`
+            mem_max=0
+            source /etc/profile && conda activate base && cd ~/graph-benchmarks && bash run_profiler.sh code/${code} data/${data} ${repetition} graph_benchmark_time_tmp.txt &
+            sleep 10
 
-        runpid=`ps -ef | grep run_profiler.sh | grep code | awk '{print $2}'`
-        while [ ${runpid} ];do
             runpid=`ps -ef | grep run_profiler.sh | grep code | awk '{print $2}'`
-            sleep 1
-            mem_cur=`free | awk '/Mem/ {print $3}'`
-            if [ ${mem_cur} -gt ${mem_max} ];then
-                mem_max=${mem_cur}
-            fi
+            while [ ${runpid} ];do
+                runpid=`ps -ef | grep run_profiler.sh | grep code | awk '{print $2}'`
+                sleep 1
+                mem_cur=`free | awk '/Mem/ {print $3}'`
+                if [ ${mem_cur} -gt ${mem_max} ];then
+                    mem_max=${mem_cur}
+                fi
+            done
+            used_kB=`expr ${mem_max} - ${mem_base}`
+            line="${line},${used_kB}"
+            sleep 10
         done
-        used_kB=`expr ${mem_max} - ${mem_base}`
-        line="${line},${used_kB}"
-        sleep 10
-    done
-    echo "${line}"
-    echo "${line}" >> ${outfile}
+        echo "${line}"
+        echo "${line}" >> ${outfile}
+        done
     done
 done
