434b205aefaf810ec635b1c2485beb9265d9ffea
abin <1506376703@qq.com>
2021-08-08 11:16:10 +0800

    update files

--- exp/graph_benchmark_memory.sh
+++ exp/graph_benchmark_memory.sh
@@ -30,7 +30,12 @@ for code in ${codes[*]};do
                 mem_base=`free | awk '/Mem/ {print $3}'`
                 mem_max=0
 
-                source /etc/profile && conda activate base && cd ~/graph-benchmarks && bash run_profiler.sh code/${code} data/${data} ${repetition} ${curdir}/${tmpdir}/graph_benchmark_time_${cname}_${dname}_${repetition}.txt &
+                tmp_outfile="${curdir}/${tmpdir}/graph_benchmark_time_${cname}_${dname}_${repetition}_times${i}.txt"
+                if [ -f ${tmp_outfile} ];then
+                    echo "${tmp_outfile} existed."
+                    continue
+                fi
+                source /etc/profile && conda activate base && cd ~/graph-benchmarks && bash run_profiler.sh code/${code} data/${data} ${repetition} ${tmp_outfile} &
                 sleep 10
 
                 runpid=`ps -ef | grep run_profiler.sh | grep code | awk '{print $2}'`
