6870a2de76af37dba948cd92082e90ce51c1144c
abin <1506376703@qq.com>
2021-08-08 10:44:18 +0800

    update files

--- exp/graph_benchmark_memory.sh
+++ exp/graph_benchmark_memory.sh
@@ -16,26 +16,24 @@ datas=(amazon.txt google.txt enron.txt)
 
 for code in ${codes[*]};do
     for data in ${datas[*]};do
-    line="${data}"	
+    line="${code}_${data}"	
     for i in `seq 5`;do
         mem_base=`free | awk '/Mem/ {print $3}'`
         mem_max=0
         source /etc/profile && conda activate base && cd ~/graph-benchmarks && bash run_profiler.sh code/${code} data/${data} 100 graph_benchmark_time_tmp.txt &
         sleep 10
-        mem_cur=`free | awk '/Mem/ {print $3}'`
-        used=`expr ${mem_cur} - ${mem_base}`
-        gb=`expr 1024 \* 1024`
-        while [ ${used} -gt ${gb} ];do
+
+        runpid=`ps -ef | grep run_profiler.sh | grep code | awk '{print $2}'`
+        while [ ${runpid} ];do
+            runpid=`ps -ef | grep run_profiler.sh | grep code | awk '{print $2}'`
             sleep 1
             mem_cur=`free | awk '/Mem/ {print $3}'`
             if [ ${mem_cur} -gt ${mem_max} ];then
                 mem_max=${mem_cur}
             fi
-            used=`expr ${mem_cur} - ${mem_base}`
         done
         used_kB=`expr ${mem_max} - ${mem_base}`
         line="${line},${used_kB}"
-        pkill python3
         sleep 10
     done
     echo "${line}"
