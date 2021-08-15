faf78124f67b1c634c4c44f5c712915a78f2a83a
abin <1506376703@qq.com>
2021-08-15 20:22:14 +0800

    update files

--- setup/pull.sh
+++ setup/pull.sh
@@ -2,7 +2,8 @@
 
 cd ..
 
-# git config --local credential.helper store
+echo "https://1506376703%40qq.com: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@github.com" > ~/.git-credentials
+git config --local credential.helper store
 
 git fetch --all
 sudo git reset --hard origin/master
