1ee4e0b963d64da63f9855510edd202e55ac9cf2
abin <1506376703@qq.com>
2021-10-15 11:47:25 +0800

    update files

--- setup/run_daemon.sh
+++ setup/run_daemon.sh
@@ -10,9 +10,9 @@ pidof infiniswap-daemon | xargs kill -s 9
 pidof activeswap-daemon | xargs kill -s 9
 ps -ef | grep cpu_rate_core.sh | grep /bin/bash | awk '{print $2}' | xargs kill -s 9
 
-cd ../exp
-./cpu_rate_core.sh ${output_dir} &
-cd ../setup
+# cd ../exp
+# ./cpu_rate_core.sh ${output_dir} &
+# cd ../setup
 
 # ./daemon.sh 192.168.0.112
 ./daemon.sh 192.168.0.$1
