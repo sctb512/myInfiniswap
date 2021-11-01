ade9a8632b42185c516ee1e31448d3392b386920
abin <1506376703@qq.com>
2021-11-01 11:06:49 +0800

    update files

--- exp/finch_lxc_cpu.sh
+++ exp/finch_lxc_cpu.sh
@@ -28,28 +28,27 @@ ps -ef | grep cpu_rate_lxc.sh | grep /bin/bash | awk '{print $2}' | xargs kill -
 
 # ./cpu_rate_lxc.sh ${output_dir} ${docker_name} &
 
-
-for i in `seq 10`;do
+for i in $(seq 10); do
     sudo mkdir -p ${output_dir}/${i}
-    for local in 100 95 90 85 80 75;do
-    # for local in 70 65 60 55 50;do
-        local_mem=`expr ${total_mem} \* ${local} / 100`
+    for local in 100 95 90 85 80 75; do
+        # for local in 70 65 60 55 50;do
+        local_mem=$(expr ${total_mem} \* ${local} / 100)
 
         file="total_mem${total_mem}_local_mem${local_mem}_local${local}.txt"
 
-        if [ -f ${output_dir}/${i}/${file} ];then
+        if [ -f ${output_dir}/${i}/${file} ]; then
             continue
         fi
 
         sudo lxc config set ${docker_name} limits.memory ${local_mem}kB
-        echo $((${local_mem}*1024+32*1024*1024*1024)) | sudo tee /sys/fs/cgroup/memory/lxc/${docker_name}/memory.memsw.limit_in_bytes
+        echo $((${local_mem} * 1024 + 32 * 1024 * 1024 * 1024)) | sudo tee /sys/fs/cgroup/memory/lxc/${docker_name}/memory.memsw.limit_in_bytes
 
         sleep 10
         sudo lxc list
 
         echo "install env in lxc..."
 
-        sudo lxc exec ${docker_name} -- su ubuntu -c "cd /home/ubuntu && rm -rf ${output_dir} && rm -rf ${out_dir}"  >/dev/null 2>&1
+        sudo lxc exec ${docker_name} -- su ubuntu -c "cd /home/ubuntu && rm -rf ${output_dir} && rm -rf ${out_dir}" >/dev/null 2>&1
         sudo lxc exec ${docker_name} -- su ubuntu -c "cd /home/ubuntu && mkdir ${output_dir} && mkdir ${out_dir} && (time /usr/bin/python3 /home/ubuntu/FINCH-Clustering/python/finch.py --data-path /home/ubuntu/FINCH-Clustering/data/mnist10k/data.csv --output-path ${out_dir}) 2> ${output_dir}/${file}"
 
         sudo lxc file pull ${docker_name}/home/ubuntu/${output_dir}/${file} ./${output_dir}/${i}/
