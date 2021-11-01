092a611a95b51a0694c77bcdc98aef1572e1d28c
abin <1506376703@qq.com>
2021-11-01 11:07:09 +0800

    update files

--- exp/cpu_rate_lxc.sh
+++ exp/cpu_rate_lxc.sh
@@ -3,12 +3,12 @@
 file="$1_cpu_rate.csv"
 docker_name=$2
 
-echo "num,total,used,available,usr,sys,rate" > ${file}
+echo "num,total,used,available,usr,sys,rate" >${file}
 
 i=0
-while [ True ];do
-    echo -e "${i},`sudo lxc exec ${docker_name} -- free -b | awk 'NR==2 {printf("%s,%s,%s",$2,$3,$7)}'`\c" >> ${file}
-    echo `sudo lxc exec ${docker_name} -- top -bn 1 | grep 'Cpu(s)' | awk -F'[" "%]+' '{printf(",%s,%s,%s\n",$3,$5,$3+$5)}'` >> ${file}
+while [ True ]; do
+    echo -e "${i},$(sudo lxc exec ${docker_name} -- free -b | awk 'NR==2 {printf("%s,%s,%s",$2,$3,$7)}')\c" >>${file}
+    echo $(sudo lxc exec ${docker_name} -- top -bn 1 | grep 'Cpu(s)' | awk -F'[" "%]+' '{printf(",%s,%s,%s\n",$3,$5,$3+$5)}') >>${file}
     sleep 1
-    i=`expr ${i} + 1`
+    i=$(expr ${i} + 1)
 done
