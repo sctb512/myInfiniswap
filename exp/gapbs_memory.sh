ea605482c60d5b5aa904ea2c6461d975f4b2b20a
abin <1506376703@qq.com>
2021-11-02 22:35:17 +0800

    update files

--- exp/gapbs_memory.sh
+++ exp/gapbs_memory.sh
@@ -3,37 +3,41 @@
 gapbs_dir="/users/bin_tang/gapbs"
 
 headline="functoin"
-for i in `seq 5`;do
-	headline="${headline},${i}_memory_used(kB)"
+for i in $(seq 5); do
+    headline="${headline},${i}_memory_used(kB)"
 done
-echo "${headline}" > gapbs_memory.csv
+echo "${headline}" >gapbs_memory.csv
 
 functoins=(bc bfs cc_sv converter pr_spmv tc cc pr sssp)
 
-for func in ${functoins[*]};do
+for func in ${functoins[*]}; do
     line="${func}"
-    for i in `seq 5`;do
-        mem_base=`free | awk '/Mem/ {print $3}'`
-		mem_max=0
-		${gapbs_dir}/${func} -u 26 -n 1 &
-		sleep 10
-		mem_cur=`free | awk '/Mem/ {print $3}'`
-		used=`expr ${mem_cur} - ${mem_base}`
-		gb=`expr 1024 \* 1024`
-		mb=$((1024*200))
-		while [ ${used} -gt ${mb} ];do
-			sleep 1
-			mem_cur=`free | awk '/Mem/ {print $3}'`
-			if [ ${mem_cur} -gt ${mem_max} ];then
-				mem_max=${mem_cur}
-			fi
-			used=`expr ${mem_cur} - ${mem_base}`
-		done
-		used_kB=`expr ${mem_max} - ${mem_base}`
-		line="${line},${used_kB}"
+    for i in $(seq 5); do
+        mem_base=$(free | awk '/Mem/ {print $3}')
+        mem_max=0
+        if [ "${func}" == "converter" ]; then
+            ${gapbs_dir}/${func} -u 26 &
+        else
+            ${gapbs_dir}/${func} -u 26 -n 1 &
+        fi
+        sleep 10
+        mem_cur=$(free | awk '/Mem/ {print $3}')
+        used=$(expr ${mem_cur} - ${mem_base})
+        gb=$(expr 1024 \* 1024)
+        mb=$((1024 * 200))
+        while [ ${used} -gt ${mb} ]; do
+            sleep 1
+            mem_cur=$(free | awk '/Mem/ {print $3}')
+            if [ ${mem_cur} -gt ${mem_max} ]; then
+                mem_max=${mem_cur}
+            fi
+            used=$(expr ${mem_cur} - ${mem_base})
+        done
+        used_kB=$(expr ${mem_max} - ${mem_base})
+        line="${line},${used_kB}"
         pkill ${func}
-		sleep 10
+        sleep 10
     done
     echo "${line}"
-	echo "${line}" >> finch_memory.csv
+    echo "${line}" >>gapbs_memory.csv
 done
