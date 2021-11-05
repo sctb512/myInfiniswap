474c0999e687f7a6acfd380c287c39f42efeed71
abin <1506376703@qq.com>
2021-11-05 21:10:42 +0800

    update files

--- exp/gapbs_lxc_cpu.sh
+++ exp/gapbs_lxc_cpu.sh
@@ -13,15 +13,12 @@ if [ ! -d ${output_dir} ]; then
     mkdir -p ${output_dir}
 fi
 
-# total_mem=19092340
-# total_mem=5287770
-
 docker_name=is-workloads
 echo "total_mem: ${total_mem}"
 
-# cd ../setup
-# ./run_infiniswap.sh ${servers_num}
-# cd ../exp
+cd ../setup
+./run_infiniswap.sh ${servers_num}
+cd ../exp
 
 sudo lxc start ${docker_name}
 
@@ -30,8 +27,6 @@ sudo lxc config set ${docker_name} limits.memory.swap true
 
 ps -ef | grep cpu_rate_lxc.sh | grep /bin/bash | awk '{print $2}' | xargs kill -s 9
 
-# ./cpu_rate_lxc.sh ${output_dir} ${docker_name} &
-
 
 for i in ${!functions[@]};do
     function=${functions[${i}]}
@@ -39,7 +34,11 @@ for i in ${!functions[@]};do
 
     echo "function: ${function}, total_memory: ${total_memory}"
 
+    ps -ef | grep cpu_rate_lxc.sh | grep "/bin/bash" | awk '{print $2}' | xargs kill -9
+    ./cpu_rate_lxc.sh ${function} ${docker_name} &
+
     cur_output_dir=${output_dir}/${function}
+
     if [ ! -d ${cur_output_dir} ]; then
         mkdir -p ${cur_output_dir}
     fi
@@ -48,6 +47,13 @@ for i in ${!functions[@]};do
         sudo mkdir -p ${cur_output_dir}/${i}
         for local in 100 95 90 85 80 75; do
             # for local in 70 65 60 55 50;do
+
+            chunk_num=$(dmesg | grep "\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*" | wc -l)
+
+            if [ ${chunk_num} -gt 28 ]; then
+                sudo reboot
+            fi
+
             local_mem=$(expr ${total_memory} \* ${local} / 100)
 
             file="total_mem${total_memory}_local_mem${local_mem}_local${local}.txt"
