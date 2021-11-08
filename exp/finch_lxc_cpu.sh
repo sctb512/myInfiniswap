91bfe6e5ec726981ad5acffb649e2bed4f605883
abin <1506376703@qq.com>
2021-11-08 22:22:39 +0800

    update files

--- exp/finch_lxc_cpu.sh
+++ exp/finch_lxc_cpu.sh
@@ -31,7 +31,8 @@ ps -ef | grep cpu_rate_lxc.sh | grep /bin/bash | awk '{print $2}' | xargs kill -
 
 ./cpu_rate_lxc.sh ${output_dir} ${docker_name} &
 
-for i in $(seq 10); do
+# for i in $(seq 10); do
+for i in $(seq 2); do
     sudo mkdir -p ${output_dir}/${i}
     for local in 100 95 90 85 80 75 70 65 60; do
         # for local in 55 50; do
