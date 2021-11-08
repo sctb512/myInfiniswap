8c58e6367011aabeab829badd7ac44537a521389
abin <1506376703@qq.com>
2021-11-08 22:19:42 +0800

    update files

--- exp/cpu_rate_lxc.sh
+++ exp/cpu_rate_lxc.sh
@@ -3,9 +3,13 @@
 file="$1_cpu_rate.csv"
 docker_name=$2
 
-echo "num,total,used,available,usr,sys,rate" >${file}
+if [ ! -f ${file} ];then
+    echo "num,total,used,available,usr,sys,rate" >${file}
+    i=0
+else
+    i=$(tail ${file} -n 1 | awk -F, '{print $1}')
+fi
 
-i=0
 while [ True ]; do
     echo -e "${i},$(sudo lxc exec ${docker_name} -- free -b | awk 'NR==2 {printf("%s,%s,%s",$2,$3,$7)}')\c" >>${file}
     echo $(sudo lxc exec ${docker_name} -- top -bn 1 | grep 'Cpu(s)' | awk -F'[" "%]+' '{printf(",%s,%s,%s\n",$3,$5,$3+$5)}') >>${file}
