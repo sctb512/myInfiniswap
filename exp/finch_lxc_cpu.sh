92c17dc29cc1b70c88fb4dde6a2cad1cce4d83e3
abin <1506376703@qq.com>
2021-11-13 10:29:51 +0800

    update files

--- exp/finch_lxc_cpu.sh
+++ exp/finch_lxc_cpu.sh
@@ -40,8 +40,8 @@ ps -ef | grep cpu_rate_lxc.sh | grep /bin/bash | awk '{print $2}' | xargs kill -
 # for i in $(seq 10); do
 for i in $(seq 2); do
     sudo mkdir -p ${output_dir}/${i}
-    for local in 100 95 90 85 80 75 70 65 60; do
-        # for local in 55 50; do
+    for local in 100 95 90 85 80 75 70; do
+        # for local in 65 60 55 50; do
         chunk_num=$(dmesg | grep "\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*" | wc -l)
 
         if [ ${chunk_num} -gt 27 ]; then
