9df1a729ce5c6c985dde8aa542b3af4b01f20b19
abin <1506376703@qq.com>
2021-11-18 10:36:39 +0800

    update files

--- exp/watch_file_num.sh
+++ exp/watch_file_num.sh
@@ -4,8 +4,8 @@ dir=$1
 file_num=$(ls -lR ${dir} |grep "^-" | wc -l)
 
 while true;do
-    echo "sleep $((60*60*2))s..."
-    sleep $((60*60*2))
+    echo "sleep $((60*60*1))s..."
+    sleep $((60*60*1))
     cur_file_num=$(ls -lR ${dir} |grep "^-" | wc -l)
 
     if [ ${cur_file_num} -le ${file_num} ];then
