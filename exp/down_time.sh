267801680f0505cc26fccc526c2016b08e4b9cea
abin <1506376703@qq.com>
2021-12-16 21:47:15 +0800

    update files

---
+++ exp/down_time.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+servers_num=$1
+
+down_dir="down"
+nodown_dir="nodown"
+
+cd ../setup
+./run_infiniswap.sh ${servers_num}  ./config1.sh
+cd ../exp
+
+
+echo "StrictHostKeyChecking no" > ~/.ssh/config
+
+for RW in randread randwrite read write;do
+    for IODEPTH in 1 4 16 32 64;do
+        for DAEMON in down nodown;do
+            if [ -f ${DAEMON}/${RW}-${IODEPTH}.txt ]; then
+                echo "file ${DAEMON}/${RW}-${IODEPTH}.txt existed, continue..."
+                continue
+            fi
+            nohup sudo fio -bs=4k -rw=${RW} -ioengine=libaio -numjobs=1 -runtime=60 -iodepth=${OIDEPTH} -filename=/dev/infiniswap0 -name=${RW}-${IODEPTH} -ramp_time=10 -output=${DAEMON}/${RW}-${IODEPTH}.txt &
+            if [ "${DAEMON}" == "down" ];then
+                sleep 30
+                source ./config1.sh ${servers_num}
+
+                for i in ${!servers[@]};do
+                    server=${servers[${i}]}
+                    ip=${ips[${i}]}
+                    ib=${ibs[${i}]}
+
+                    echo "cp-${server}, ib: 192.168.0.${ib}, reboot..."
+                    ssh ${user}@128.110.96.${ip} "ps -ef | grep run_daemon.sh | grep /bin/bash | awk '{print \$2}' | xargs kill -s 9"
+                    sleep 2
+                    ssh ${user}@128.110.96.${ip} "reboot" 
+                done
+            fi
+            echo "rw: ${RW}, iodepth: ${IODEPTH} running..."
+            sleep 100
+
+            sudo reboot
+        done
+    done
+done
