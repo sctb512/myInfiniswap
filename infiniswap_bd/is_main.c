327696258358db20e3d3fa4b782cc413d7fc59ae
abin <1506376703@qq.com>
2021-12-19 21:35:39 +0800

    update files

--- infiniswap_bd/is_main.c
+++ infiniswap_bd/is_main.c
@@ -1893,8 +1893,8 @@ int IS_single_chunk_map(struct IS_session *IS_session, int select_chunk)
 
 	simulate_select(IS_session, select_chunk, 10);
 	simulate_select(IS_session, select_chunk, 100);
-	// simulate_select(IS_session, select_chunk, 1000);
-	// simulate_select(IS_session, select_chunk, 10000);
+	simulate_select(IS_session, select_chunk, 1000);
+	simulate_select(IS_session, select_chunk, 10000);
 	// simulate_select(IS_session, select_chunk, 1000000);
 	// simulate_select(IS_session, select_chunk, 10000000);
 
