412e4940893d8a1bd87b3f0365bf7b60563781c4
abin <1506376703@qq.com>
2021-12-28 20:54:34 +0800

    update files

---
+++ exp/cpu_rate.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ $# != 3 ];then
+    echo "useage: $0 file_name dockere_name file_dir"
+    exit
+fi
+
+dir=$3
+file="${dir}/$1_cpu_rate.csv"
+docker_name=$2
+
+interval=1
+
+start_idle=()
+start_total=()
+cpu_rate=()
+
+
+echo -n "turn," > ${cpu_rate_file}
+
+echo -n "cpu_avg,cpu_user,cpu_sys,mem_rate" >> ${cpu_rate_file}
+echo "" >> ${cpu_rate_file}
+
+turn=0
+while [ True ];do
+    echo -n "${turn}," >> ${cpu_rate_file}
+
+    start=$(cat /proc/stat | grep "cpu " | awk '{print $2" "$3" "$4" "$5" "$6" "$7" "$8}')
+
+    start_idle=$(echo ${start} | awk '{print $4}')
+    start_total=$(echo ${start} | awk '{printf "%.f",$1+$2+$3+$4+$5+$6+$7}')
+    start_user=$(echo ${start} | awk '{print $1}')
+    start_sys=$(echo ${start} | awk '{print $3}')
+    sleep ${interval}
+    
+    end=$(cat /proc/stat | grep "cpu " | awk '{print $2" "$3" "$4" "$5" "$6" "$7" "$8}')
+    end_idle=$(echo ${end} | awk '{print $4}')
+    end_total=$(echo ${end} | awk '{printf "%.f",$1+$2+$3+$4+$5+$6+$7}')
+    end_user=$(echo ${end} | awk '{print $1}')
+    end_sys=$(echo ${end} | awk '{print $3}')
+
+    idle=`expr ${end_idle} - ${start_idle}`
+    total=`expr ${end_total} - ${start_total}`
+    user=`expr ${end_user} - ${start_user}`
+    sys=`expr ${end_sys} - ${start_sys}`
+
+    idle_normal=`expr ${idle} \* 100`
+    user_normal=`expr ${user} \* 100`
+    sys_normal=`expr ${sys} \* 100`
+
+    cpu_usage=`expr ${idle_normal} / ${total}`
+    cpu_rate=`expr 100 - ${cpu_usage}`
+    user_rate=`expr ${user_normal} / ${total}`
+    sys_rate=`expr ${sys_normal} / ${total}`
+
+    echo -n "${cpu_rate},${user_rate},${sys_rate}," >> ${cpu_rate_file}
+
+    mem_rate=`free -m | awk -F '[ :]+' 'NR==2{printf("%.2f\n"), $3/$2*100}'`
+    echo -n "${mem_rate}" >> ${cpu_rate_file}
+    echo "" >> ${cpu_rate_file}
+    turn=`expr ${turn} + 1`
+done
---
+++ exp/memcached_lxc_cpu.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+servers_num=$1
+
+output_dir="is_lxc_result_memcached_${servers_num}"
+cpu_rate_dir="${output_dir}_cpu_rate"
+
+if [ ! -d ${output_dir} ]; then
+    mkdir -p ${output_dir}
+fi
+if [ ! -d ${cpu_rate_dir} ]; then
+    mkdir -p ${cpu_rate_dir}
+fi
+
+total_mem=1460099
+docker_name=is-workloads
+echo "total_mem: ${total_mem}"
+
+eval $(ssh-agent -s)
+ssh-add /users/bin_tang/.ssh/cloud
+
+# echo 0 | sudo tee  /proc/sys/kernel/hung_task_timeout_secs
+
+cd ../setup
+./run_infiniswap.sh ${servers_num} ./config1.sh
+cd ../exp
+
+sudo lxc start ${docker_name}
+
+sudo lxc config set ${docker_name} limits.memory.swap.priority 50
+sudo lxc config set ${docker_name} limits.memory.swap true
+
+ps -ef | grep cpu_rate.sh | grep /bin/bash | awk '{print $2}' | xargs kill -s 9
+
+./cpu_rate.sh ${output_dir} ${docker_name} ${cpu_rate_dir} &
+./watch_file_num.sh ${output_dir} &
+
+for i in $(seq 10); do
+# for i in $(seq 2); do
+    sudo mkdir -p ${output_dir}/${i}
+    for local in 100 95 90 85 80 75 70; do
+        # for local in 65 60 55 50; do
+        chunk_num=$(dmesg | grep "\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*" | wc -l)
+
+        if [ ${chunk_num} -gt $((27*${servers_num})) ]; then
+            sudo reboot
+            exit
+        fi
+
+        local_mem=$(expr ${total_mem} \* ${local} / 100)
+
+        file="total_mem${total_mem}_local_mem${local_mem}_local${local}.txt"
+
+        if [ -f ${output_dir}/${i}/${file} ]; then
+            echo "file ${file} existed, continue..."
+            continue
+        fi
+        echo "time: $(date "+%Y-%m-%d %H:%M:%S")"
+
+        sudo lxc config set ${docker_name} limits.memory ${local_mem}kB
+        echo $((${local_mem} * 1024 + 32 * 1024 * 1024 * 1024)) | sudo tee /sys/fs/cgroup/memory/lxc/${docker_name}/memory.memsw.limit_in_bytes
+
+        sleep 10
+        sudo lxc list
+
+        echo "install env in lxc..."
+
+        sudo lxc exec ${docker_name} -- sudo --login --user root /usr/bin/zsh -ic "cd /root && rm -rf ${output_dir}" >/dev/null 2>&1
+
+        echo "total: ${total_mem} local: ${local} running..."
+        sudo lxc exec ${docker_name} -- sudo --login --user root /usr/bin/zsh -ic "cd /root && mkdir ${output_dir} && memcached -p 11211 -m 64m -d -u root && sleep 5 && memtier_benchmark -s 127.0.0.1 -p 11211 -P memcache_text --clients 100 --threads 8 --data-size 256 --test-time 30 --pipeline 10240 --out-file > ${output_dir}/${file} 2>&1"
+
+        sudo lxc file pull ${docker_name}/root/${output_dir}/${file} ./${output_dir}/${i}/
+
+    done
+done
