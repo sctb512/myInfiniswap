5def15c380c5914c0c4437a8debe0d65cc18bb73
abin <1506376703@qq.com>
2021-12-29 09:15:10 +0800

    update files

--- exp/memcached_lxc_cpu.sh
+++ exp/memcached_lxc_cpu.sh
@@ -38,7 +38,7 @@ ps -ef | grep cpu_rate.sh | grep /bin/bash | awk '{print $2}' | xargs kill -s 9
 for i in $(seq 10); do
 # for i in $(seq 2); do
     sudo mkdir -p ${output_dir}/${i}
-    for local in 100 95 90 85 80 75 70; do
+    for local in 100 95 90 85 80 75 70 65 60 55 50; do
         # for local in 65 60 55 50; do
         chunk_num=$(dmesg | grep "\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*" | wc -l)
 
