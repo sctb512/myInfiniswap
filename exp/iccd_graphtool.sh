cc012fc82bfc35f9cf3dcf6b587c8358bfe12dd1
abin <1506376703@qq.com>
2022-05-31 22:02:48 +0800

    update files

--- exp/iccd_graphtool.sh
+++ exp/iccd_graphtool.sh
@@ -50,7 +50,7 @@ eval $(ssh-agent -s)
 ssh-add /users/bin_tang/.ssh/cloud
 
 cd ../setup
-./run_infiniswap.sh ${servers_num}  ./config2.sh ${output_dir} ${cpu_rate_dir}
+./run_infiniswap.sh ${servers_num}  ./config1.sh ${output_dir} ${cpu_rate_dir}
 cd ../exp
 
 sudo lxc start ${docker_name}
