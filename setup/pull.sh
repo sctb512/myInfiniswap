5c9cd2c7dec9b97de20ae82b6129e9fffdcdfe65
abin <1506376703@qq.com>
2022-06-09 11:43:13 +0800

    update files

--- setup/pull.sh
+++ setup/pull.sh
@@ -4,7 +4,10 @@ cd ..
 
 # echo "https://1506376703%40qq.com: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@github.com" > ~/.git-credentials
 # git config --global credential.helper store
+# sudo git config --global credential.helper store
 
-# git fetch --all
-# sudo git reset --hard origin/master
-git pull --all -f
+# git pull --all -f
+
+git branch -r | grep -v '\->' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
+git fetch --all
+git pull --all
