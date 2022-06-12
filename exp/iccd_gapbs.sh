7897ca6a08f788c7554a77f931b08727217c4bab
abin <1506376703@qq.com>
2022-06-12 10:56:04 +0800

    update files

--- exp/iccd_gapbs.sh
+++ exp/iccd_gapbs.sh
@@ -21,7 +21,7 @@ gapbs_dir="/root/gapbs"
 # total_memorys=(18186932 18188654 18132460 18232750 18218126 18133960 18131084 33498212)
 functions=(cc_sv pr_spmv cc sssp)
 # total_memorys=(18132460 18232750 18133960 33498212)
-total_memorys=(4561044,4550048,4576217,8523690)
+total_memorys=(4561044 4550048 4576217 8523690)
 
 
 if [ ! -d ${output_dir} ]; then
